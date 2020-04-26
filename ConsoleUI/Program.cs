2f7b53485d48d9a4ede117bfb33959dec90de561
Adam <adamszulc1991@gmail.com>
2020-04-26 19:02:49 +0200

    Initial commit - solved basic case.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+            for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+                    Console.WriteLine(result.TransactionAmount);
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine($"Null  for payment number {i}.");
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    if (ex is IndexOutOfRangeException || ex is FormatException)
+                    {
+                        Console.WriteLine($"Payment skipped for payment with {i} items.");
+                        continue;
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
